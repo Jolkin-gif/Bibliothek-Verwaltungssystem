4ac7f2244ab8aa88632db70bf26b21f1dcc977b8
Jolkin-gif <Daniel.Jolkin@gmail.com>
2025-12-02 15:22:53 +0100

    Projektdateien hinzufügen.

---
+++ Bibliothek-Verwaltungssystem/Buch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliothek_Verwaltungssystem
+{
+    public class Buch
+    {
+        public string Titel { get; set; }
+        public string Autor { get; set; }
+        public string Status { get; set; } = "Verfügbar";
+        public string Ausleiher { get; set; }
+        public DateTime? Ausleihdatum { get; set; }
+        public bool Verlängert { get; set; } = false;
+
+        public Buch()
+        {
+            Status = "Verfügbar";
+            Verlängert = false;
+        }
+        public string StatusFarbe()
+        {
+            if (Status == "Verloren")
+                return "VERLOREN";
+
+            if (Status == "Verfügbar")
+                return "VERFÜGBAR";
+
+            if (!Ausleihdatum.HasValue)
+                return "FEHLER";
+
+            TimeSpan differenz = DateTime.Now - Ausleihdatum.Value;
+
+            if (differenz.TotalDays <= 30)
+                return "Grün";
+
+            if (Verlängert && differenz.TotalDays <= 44)
+                return "Gelb";
+
+            return "Rot";
+
+
+
+
+        }
+    }
+}
